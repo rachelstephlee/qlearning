bad4786be151245393239b2d9f3b18bbafd1d099
Rachel Lee (PNI) <rslee@princeton.edu>
2019-03-05 20:46:09 -0500

    finished all review responses analyses for resubmission. hurrah

--- Reviews_analyses.jl
+++ Reviews_analyses.jl
@@ -7,6 +7,8 @@ using MixedModels
 using StatsBase
 using DataFrames
 using Plots
+using MultipleTesting
+
 
 function normalize(x, mu, std)
     return (x .- mu) ./ std
@@ -29,19 +31,6 @@ end
 
 # formats the vector so that it's floats and not strings (for the gcamp)
 format_vec = str_vec->[parse(Float64, ss) for ss in split(str_vec[2:(end-1)])]
-
-############################################
-# EARLY VS LATE BLOCKS
-############################################
-
-
-RECORDLOC = "DMS"
-BLOCK = "late"   # early or late
-Q_VALS_TYPE = :Q_ch_diff
-TIMELOCK = :g_lp
-
-df_src = CSV.read("data/" * RECORDLOC * "/df_" * BLOCK * ".csv")
-
 
 ##### prep for regression #####
 # function prepares regression by: 1. correctly pull out timelocked data to float form 2. normalize q values
@@ -68,6 +57,35 @@ function prepare_regression(df_src, timelock, q_vals_type)
 
     return df_reg
 end
+
+
+function correct_pvals(df_fits)
+
+    for var = unique(df_fits[:Variable])
+        println(var)
+        pvals = convert(Array{Float64,1}, df_fits[df_fits[:Variable] .== var, :Pval])
+        df_fits[df_fits[:Variable] .== var, :Pval] = adjust(PValues(pvals),MultipleTesting.BenjaminiHochberg())
+
+    end
+
+    return df_fits
+
+end
+
+############################################
+# EARLY VS LATE BLOCKS
+############################################
+
+
+RECORDLOC = "DMS"
+BLOCK = "late"   # early or late
+Q_VALS_TYPE = :Q_ch_diff
+TIMELOCK = :g_lp
+
+df_src = CSV.read("data/" * RECORDLOC * "/df_" * BLOCK * ".csv")
+
+
+
 
 df_reg = prepare_regression(df_src, TIMELOCK, Q_VALS_TYPE)
 
@@ -100,24 +118,11 @@ df_fits = CSV.read("data/" * RECORDLOC * "/df_" * BLOCK * "_reg.csv")
 
 ### correcting p-values
 
-using MultipleTesting
 
 
 RECORDLOC = "DMS"
 BLOCK = "early"
 
-function correct_pvals(df_fits)
-
-    for var = unique(df_fits[:Variable])
-        println(var)
-        pvals = convert(Array{Float64,1}, df_fits[df_fits[:Variable] .== var, :Pval])
-        df_fits[df_fits[:Variable] .== var, :Pval] = adjust(PValues(pvals),MultipleTesting.BenjaminiHochberg())
-
-    end
-
-    return df_fits
-
-end
 
 
 
@@ -156,6 +161,133 @@ function pull_regression_coefs(df_fits, fit, recordloc, stayvsleave, var_names)
     return df_i
 end
 
+
+#############
+# LATENCY: Adding it to the main regression
+#############
+
+RECORDLOC = "DMS"
+Q_VALS_TYPE = :Q_ch_diff
+TIMELOCK = :g_lp
+
+df_corr_all = CSV.read("data/int_pc_qvals_gcamp_dms (1).csv", index_col = 0) # .dropna(subset=['Q_dir_diff'])
+
+
+
+df_src = df_corr_all[(df_corr_all[:RecordLoc] .== RECORDLOC) , :]
+
+
+df_reg = prepare_regression(df_src, TIMELOCK, Q_VALS_TYPE)
+
+# add latency information to df_reg
+
+df_latency = CSV.read("data/latency_qvals_gcamp.csv")
+
+
+df_full = join(df_src, df_latency[(df_latency[:RecordLoc] .== RECORDLOC) , :], on = [:MouseID, :Trial, :RecordLoc, :Session], makeunique= true)
+df_reg[:Latency_choice] = df_full[:Latency_choice]
+
+
+### perform regression
+df_fits = DataFrame(Estimate = Float64[], StdError = Float64[], Pval = Float64[], Variable = String[],
+                       Time = Float64[], Timelock = String[], RecordLoc = String[])
+for i in 1:45
+   lhs = Symbol("x", i)
+   formula_i = @eval @formula($lhs ~ 1 + Action_float + $Q_VALS_TYPE + Interact + Latency_choice + (1 + Action_float + $Q_VALS_TYPE + Interact  + Latency_choice  | MouseID))
+   fits_i = fit(LinearMixedModel, formula_i, df_reg)
+   co = coef(fits_i)
+   se = stderror(fits_i) # assuming this is calculating standard error
+   z = co ./ se
+   pval = ccdf.(Chisq(1), abs2.(z))
+
+   df_i = DataFrame()
+   df_i.Estimate = co
+   df_i.StdError = se
+   df_i.Pval = pval
+   df_i.Variable = ["Intercept", "Action:Con", string(Q_VALS_TYPE), "Interact", "Lat_choice"]
+   df_i.Time = ones(length(df_i.Variable)) .* (-1 + 3/45 * (i - 1))
+   df_i.Timelock = string(TIMELOCK)
+   df_i.RecordLoc = RECORDLOC
+
+
+   append!(df_fits, df_i)
+end
+
+# p value correcting
+
+
+
+df_fits = correct_pvals(df_fits)
+
+CSV.write("data/" * RECORDLOC * "/Julia_lever_latency_corrected.csv", df_fits)
+
+
+#############
+# LATENCY: Adding it to the main regression (CONTRA ONLY)
+#############
+
+RECORDLOC = "DMS_CB"
+Q_VALS_TYPE = :Q_ch_diff
+TIMELOCK = :g_lp
+
+df_corr_all = CSV.read("data/int_pc_qvals_gcamp_dms (1).csv", index_col = 0) # .dropna(subset=['Q_dir_diff'])
+
+
+# df_src = df_corr_all[df_corr_all[:RecordLoc] .== RECORDLOC, :]
+# only checking contralateral choices!
+print("Only Contra choices") #
+df_src = df_corr_all[(df_corr_all[:RecordLoc] .== RECORDLOC) .& (df_corr_all[:Action] .== "Con") , :]
+
+
+df_reg = prepare_regression(df_src, TIMELOCK, Q_VALS_TYPE)
+
+# add latency information to df_reg
+
+df_latency = CSV.read("data/latency_qvals_gcamp.csv")
+
+print("Only Contra Choices") #
+df_full = join(df_src, df_latency[(df_latency[:RecordLoc] .== RECORDLOC) .& (df_latency[:Action] .== "Con"), :], on = [:MouseID, :Trial, :RecordLoc, :Session], makeunique= true)
+df_reg[:Latency_choice] = df_full[:Latency_choice]
+
+
+### perform regression
+df_fits = DataFrame(Estimate = Float64[], StdError = Float64[], Pval = Float64[], Variable = String[],
+                       Time = Float64[], Timelock = String[], RecordLoc = String[])
+for i in 1:45
+   lhs = Symbol("x", i)
+   formula_i = @eval @formula($lhs ~ 1 + $Q_VALS_TYPE +  Latency_choice + (1 +  $Q_VALS_TYPE + Latency_choice  | MouseID))
+   fits_i = fit(LinearMixedModel, formula_i, df_reg)
+   co = coef(fits_i)
+   se = stderror(fits_i) # assuming this is calculating standard error
+   z = co ./ se
+   pval = ccdf.(Chisq(1), abs2.(z))
+
+   df_i = DataFrame()
+   df_i.Estimate = co
+   df_i.StdError = se
+   df_i.Pval = pval
+   df_i.Variable = ["Intercept", string(Q_VALS_TYPE), "Lat_choice"]
+   df_i.Time = ones(length(df_i.Variable)) .* (-1 + 3/45 * (i - 1))
+   df_i.Timelock = string(TIMELOCK)
+   df_i.RecordLoc = RECORDLOC
+
+
+   append!(df_fits, df_i)
+end
+
+# p value correcting
+
+
+
+df_fits = correct_pvals(df_fits)
+
+CSV.write("data/" * RECORDLOC * "/Julia_lever_latency_corrected_CONTRA.csv", df_fits)
+
+
+#################################
+# LATENCY: choice
+#################################
+
 # Lmer(latency ~ 1 + Qchdiff + presLat + (1 + Qchdiff + Preslat | mouseid), …)
 Q_VALS_TYPE = :Q_ch_diff
 df = CSV.read("data/latency_qvals_gcamp.csv")
@@ -165,9 +297,7 @@ df[:StayVSLeave] = coalesce.(df[:StayVSLeave], "na")
 histogram(df[:Latency_prez])
 histogram(df[:Latency_choice])
 histogram(df[:Latency_np])
-#################################
-# LATENCY: choice
-#################################
+
 
 df[:l_Latency_prez] = log.(df[:Latency_prez])
 df[:l_Latency_choice] = log.(df[:Latency_choice])
@@ -244,61 +374,3 @@ latency_fits_leave = fit(LinearMixedModel, formula_i, df_DMS_leave)
 latency_fits_CB = fit(LinearMixedModel, formula_i, df_DMS_CB)
 latency_fits_stay_CB = fit(LinearMixedModel, formula_i, df_DMS_CB_stay)
 latency_fits_leave_CB = fit(LinearMixedModel, formula_i, df_DMS_CB_leave)
-
-
-#############
-# LATENCY: Adding it to the main regression
-#############
-
-RECORDLOC = "DMS_CB"
-Q_VALS_TYPE = :Q_ch_diff
-TIMELOCK = :g_lp
-
-df_corr_all = CSV.read("data/int_pc_qvals_gcamp_dms (1).csv", index_col = 0) # .dropna(subset=['Q_dir_diff'])
-
-
-df_src = df_corr_all[df_corr_all[:RecordLoc] .== RECORDLOC, :]
-
-
-df_reg = prepare_regression(df_src, TIMELOCK, Q_VALS_TYPE)
-
-# add latency information to df_reg
-
-df_latency = CSV.read("data/latency_qvals_gcamp.csv")
-
-df_full = join(df_src, df_latency[df_latency[:RecordLoc] .== RECORDLOC, :], on = [:MouseID, :Trial, :RecordLoc, :Session], makeunique= true)
-df_reg[:Latency_choice] = df_full[:Latency_choice]
-
-
-### perform regression
-df_fits = DataFrame(Estimate = Float64[], StdError = Float64[], Pval = Float64[], Variable = String[],
-                       Time = Float64[], Timelock = String[], RecordLoc = String[])
-for i in 1:45
-   lhs = Symbol("x", i)
-   formula_i = @eval @formula($lhs ~ 1 + Action_float + $Q_VALS_TYPE + Interact + Latency_choice + (1 + Action_float + $Q_VALS_TYPE + Interact  + Latency_choice  | MouseID))
-   fits_i = fit(LinearMixedModel, formula_i, df_reg)
-   co = coef(fits_i)
-   se = stderror(fits_i) # assuming this is calculating standard error
-   z = co ./ se
-   pval = ccdf.(Chisq(1), abs2.(z))
-
-   df_i = DataFrame()
-   df_i.Estimate = co
-   df_i.StdError = se
-   df_i.Pval = pval
-   df_i.Variable = ["Intercept", "Action:Con", string(Q_VALS_TYPE), "Interact", "Lat_choice"]
-   df_i.Time = ones(length(df_i.Variable)) .* (-1 + 3/45 * (i - 1))
-   df_i.Timelock = string(TIMELOCK)
-   df_i.RecordLoc = RECORDLOC
-
-
-   append!(df_fits, df_i)
-end
-
-# p value correcting
-
-
-
-df_fits = correct_pvals(df_fits)
-
-CSV.write("data/" * RECORDLOC * "/Julia_lever_latency_corrected.csv", df_fits)
